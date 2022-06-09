fce04ad119f1706ce7b68553e981048214b16c68
Katios <kate.chonkina@gmail.com>
2022-06-09 13:59:01 +0600

    First commit

---
+++ MissingPet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MissingPet
+//
+//  Created by Екатерина Чернова on 09.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MissingPet/MissingPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissingPetApp.swift
+//  MissingPet
+//
+//  Created by Екатерина Чернова on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MissingPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
