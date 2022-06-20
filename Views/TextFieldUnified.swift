cfda40ee532fd001b0a0cac267f1e819678b2822
Katios <kate.chonkina@gmail.com>
2022-06-20 17:41:08 +0600

    fix missing view

--- Views/TextFieldUnified.swift
+++ Views/TextFieldUnified.swift
@@ -13,8 +13,7 @@ struct TextFieldUnified: View {
                 isEmailWrong ?
                   RoundedRectangle(cornerRadius: 5)
                       .stroke(Color.red, lineWidth: 2)
-                : nil
-              )
+                : nil)
             .cornerRadius(5.0)
             .padding(.vertical, 10)
             .shadow(radius: 3)
